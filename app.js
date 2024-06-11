c58293811bdd1441267ea2dc9b62db9937422cf1
ashnadsilva23 <ashnadsilva2309@gmail.com>
2024-06-11 23:05:11 +0530

    project completed

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+const {employeemodel} = require("./models/addEmploy")
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect("mongodb+srv://ashna:ashna@cluster0.n9qo4.mongodb.net/employeeDB?retryWrites=true&w=majority&appName=Cluster0")
+
+
+app.post("/add", (req, res) => {
+    let input = req.body
+    let employee = new employeemodel(input)
+    employee.save()
+    res.json({"status":"success"})
+
+
+})
+app.get("/view", (req, res) => {
+    employeemodel.find().then(
+        (data)=>{
+            res.json(data)
+
+        }
+    ).catch(
+        (error)=>{
+            res.json(error)
+        }
+    )
+})
+
+app.listen(8082,() => {
+    console.log("server started")
+})
